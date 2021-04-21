ec1ceb338faeeba0ba86adaea5c20fd52b4f0488
Hiroyuki KOBAYASHI <hkob@metro-cit.ac.jp>
2021-04-21 17:50:16 +0900

    ngspice-x を追加

---
+++ ngspice-x.rb
@@ -0,0 +1,54 @@
+class NgspiceX < Formula
+  desc "Spice circuit simulator"
+  homepage "https://ngspice.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/34/ngspice-34.tar.gz"
+  sha256 "2263fffc6694754972af7072ef01cfe62ac790800dad651bc290bfcae79bd7b5"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/ngspice[._-]v?(\d+(?:\.\d+)*)\.t}i)
+  end
+
+  head do
+    url "https://git.code.sf.net/p/ngspice/ngspice.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "bison" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "fftw"
+  depends_on "readline"
+  depends_on "libxaw"
+
+  def install
+    system "./autogen.sh" if build.head?
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-readline=yes
+      --enable-xspice
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cir").write <<~EOS
+      RC test circuit
+      v1 1 0 1
+      r1 1 2 1
+      c1 2 0 1 ic=0
+      .tran 100u 100m uic
+      .control
+      run
+      quit
+      .endc
+      .end
+    EOS
+    system "#{bin}/ngspice", "test.cir"
+  end
+end
